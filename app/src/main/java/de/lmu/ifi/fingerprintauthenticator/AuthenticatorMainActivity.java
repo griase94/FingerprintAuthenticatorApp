25d4a4908632fa34e85bb50588eabaac60f9790c
Andy Griesbeck <andy.griesbeck@gmail.com>
2018-01-13 18:16:06 +0100

    Initial commit

---
+++ app/src/main/java/de/lmu/ifi/fingerprintauthenticator/AuthenticatorMainActivity.java
@@ -0,0 +1,13 @@
+package de.lmu.ifi.fingerprintauthenticator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AuthenticatorMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authenticator_main);
+    }
+}
